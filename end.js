26955d30e7b944a606c2aeaae1939ed0d9706c2b
Ann Hagan <ann.marie783@gmail.com>
2020-06-19 10:25:50 -0500

    Found the type for highScores in end.html and end.js to fix issues v.03

--- end.js
+++ end.js
@@ -1,16 +1,42 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const MAX_HIGH_SCORRES = 5;
+
+
+//things in local storage are stored as strings
+//must convert the number to a string using JSON.parse
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
+    console.log("clicked the save button!")
     e.preventDefault();
     //this stops forms from posting to another page
+
+    const score = {
+        score : Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    //the below code sorts the highest scores in local storage
+    //the arrow implies a return, therefore the brackets and return statement is not needed.
+    highScores.sort( (a,b) => b.score - a.score)
+
+    //splice the high scores so that only the top 5 show.
+    highScores.splice(5);
+
+    //below codes sets the score into local storage
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    //returns the user to home
+    window.location.assign("/");
 };
 
